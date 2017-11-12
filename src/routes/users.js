7cfd82c08fc63d612e2837e0c04200aac39c9752
Ravuthz <ravuthz@gmail.com>
2017-11-12 15:09:16 +0700

    Update users routes

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,18 +5,58 @@ import { sendConfirmationEmail } from "../mailer";
 
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  User.find({})
+    .then(users => {
+      res.json({ users });
+    })
+    .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
 router.post("/", (req, res) => {
-    const { email, password } = req.body.user;
-    const user = new User({ email });
+  const { email, password } = req.body.user;
+  const user = new User({ email });
+  user.confirmed = false;
+  user.setPassword(password);
+  user.setConfirmationToken();
+  user
+    .save()
+    .then(userSaved => {
+      sendConfirmationEmail(userSaved);
+      res.json({ user: userSaved.toAuthJSON() });
+    })
+    .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
+router.get("/:id", (req, res) => {
+  User.findById(req.params.id, (err, user) => {
+    if (err) res.status(404).json({ errors: parseErrors(err.errors) });
+    res.json({ user });
+  });
+});
+
+router.put("/:id", (req, res) => {
+  const { email, password, confirmed } = req.body.user;
+  User.findById(req.params.id, (err, user) => {
+    if (err) res.status(404).json({ errors: parseErrors(err.errors) });
+    user.email = email;
+    user.confirmed = confirmed;
     user.setPassword(password);
     user.setConfirmationToken();
     user
-        .save()
-        .then(userRecord => {
-            sendConfirmationEmail(userRecord);
-            res.json({ user: userRecord.toAuthJSON() });
-        })
-        .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+      .save()
+      .then(userSaved => {
+        res.json({ user: userSaved.toAuthJSON() });
+      })
+      .catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+  });
+});
+
+router.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id, err => {
+    if (err) res.status(404).json({ errors: parseErrors(err.errors) });
+    res.json({ success: true });
+  });
 });
 
 export default router;
